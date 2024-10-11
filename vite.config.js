d534b09788c12721363c31886b941c13165df209
Vitor <vitorsouzasilva121@gmail.com>
2024-10-11 19:50:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: mode === 'github' ? '/Vtssweb/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'github' ? '/Vtssweb/' : '/',
+  }
 })
