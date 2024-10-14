f641cc90846d243e47d4e73837c6f12c18d2d6ae
Vitor <vitorsouzasilva121@gmail.com>
2024-10-14 12:13:37 -0300

    remove gitubpages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    plugins: [react()],
-    base: mode === 'github' ? '/Vtssweb/' : '/',
+    plugins: [react()]
   }
 })
 
