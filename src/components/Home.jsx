da865df7e55382ab551e8745fb3771f8735be6d2
Vitor <vitorsouzasilva121@gmail.com>
2024-10-15 10:55:57 -0300

    add: Social media at Home.jsx and modify button more about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@ import ilustracao from '../assets/ilustracao.png';
 import seta from '../assets/seta.png';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import React, { useState } from 'react';
+import whatsapp from '../assets/whatsapp.png';
+import linkedin from '../assets/linkedin.png';
+import github from '../assets/github.png';
 
 export default function Home() {
   const [openText, setOpenText] = useState(false);
@@ -39,10 +42,22 @@ export default function Home() {
           <div className='mt-2'>
           <button
             onClick={() => setOpenText(!openText)}
-            className='text-white border-b-2 border-transparent hover:border-violet-500 px-2 sm:hidden'
+            className='text-white text-xs ml-2 border-2 rounded-md border-violet-500 hover:bg-zinc-700 px-2 py-1 sm:hidden'
           >
             {openText ? 'Ver menos' : 'Saiba mais'}
           </button>
+          
+          <div className="flex space-x-4 mt-5 sm:mt-10 items-center justify-center">
+            <a href="https://www.linkedin.com/in/vitor-souza-143b34138/" target="_blank" >
+                <img src={linkedin} alt="linkedin" className='w-6 sm:w-10 transform transition-transform duration-300 hover:scale-110'/>
+            </a>
+            <a href="https://github.com/VtssBR" target="_blank" >
+                <img src={github} alt="github" className='w-6 sm:w-10 transform transition-transform duration-300 hover:scale-110'/>
+            </a>
+            <a href="https://wa.me/5511942025733?text=Olá!" target="_blank" >
+                <img src={whatsapp} alt="whatsapp" className='w-6 sm:w-10 transform transition-transform duration-300 hover:scale-110' />
+            </a>
+          </div>
           </div>
         </div>
 
